57ff64e72588d0aab08b2c4cd5e5517c4758385f
Dan Beck <danjacobbeck@gmail.com>
2022-03-11 21:06:04 -0800

    completed models

--- models/Product.js
+++ models/Product.js
@@ -8,8 +8,41 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
+  {    // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false, 
+      primaryKey: true,
+      autoIncrement: true, 
+    },
+    category_name: {
+      type: DataTypes.STRING, 
+      allowNull: false, 
+    },
+price: {
+  type: DataTypes.DECIMAL(10,2),
+  allowNull:false,
+  validate: {
+    isDecimal: true,
+  },
+  stock: {
+    type: Datatypes.INTEGER, 
+    allowNull: false, 
+    defaultValue: 10, 
+    validate: {
+      isNumeric: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true, 
+      references: {
+        model: 'category', 
+        key: 'id',
+      },
+    },
+  },
+  
+}
   },
   {
     sequelize,
